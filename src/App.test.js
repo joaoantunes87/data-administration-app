502e395bd1e548fd317f031c1eeb14537255c877
Joao <joao.antunes@present-technologies.com>
2018-01-04 10:14:19 +0000

    Adding enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
+import { shallow } from 'enzyme';
+import App from './App';
+
+jest.mock('d2-ui/lib/app-header/HeaderBar');
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
